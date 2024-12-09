4f7e8ba66443973d3e415e0205d44dca2ee8b6e7
ThanhNL <120170567+trungthanhcva2206@users.noreply.github.com>
2024-12-09 08:00:46 +0700

    Update plugins.js

--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -1,3 +1,9 @@
+/**
+ * @Project ApibackEnd
+ * @Author TT(trungthanhcva2206@gmail.com@gmail.com)
+ * @Copyright (C) 2024 CHK. All rights reserved
+ * @License GNU/GPL version 3.0
+ */
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = () => ({});
